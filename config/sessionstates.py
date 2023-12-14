cdbcfd0b5a18cee9108fb3ef78711ca0a632933f
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 10:20:52 -0600

    Update and rename initialize_sessionstates.py to sessionstates.py

---
+++ config/sessionstates.py
@@ -0,0 +1,121 @@
+import streamlit as st
+
+storydata_template = {
+    "title": "",
+    "summary": "",
+    "pagecount": 10,
+    "character": {
+        "provided_image_url": "",
+        "character_description": "",
+        "lovedonename": "",
+        "lovedonerelationship": ""
+    },
+    "storyelements": {
+        "genre": "",
+        "setting": "",
+        "supportingcharacter": "",
+        "plotelements": "",
+        "selectedtheme": "",
+        "magicalobjects": "",
+        "tonemood": ""
+    },
+    "pages": {
+        "page1": {
+            "narrative": "",
+            "imageprompt": "",
+            "seedid": "",
+            "genid": "",
+            "imagefile": "",
+            "imageurl": ""
+        },
+        "page2": {
+            "narrative": "",
+            "imageprompt": "",
+            "seedid": "",
+            "genid": "",
+            "imagefile": "",
+            "imageurl": ""
+        },
+        "page3": {
+            "narrative": "",
+            "imageprompt": "",
+            "seedid": "",
+            "genid": "",
+            "imagefile": "",
+            "imageurl": ""
+        },
+        "page4": {
+            "narrative": "",
+            "imageprompt": "",
+            "seedid": "",
+            "genid": "",
+            "imagefile": "",
+            "imageurl": ""
+        },
+        "page5": {
+            "narrative": "",
+            "imageprompt": "",
+            "seedid": "",
+            "genid": "",
+            "imagefile": "",
+            "imageurl": ""
+        },
+        "page6": {
+            "narrative": "",
+            "imageprompt": "",
+            "seedid": "",
+            "genid": "",
+            "imagefile": "",
+            "imageurl": ""
+        },
+        "page7": {
+            "narrative": "",
+            "imageprompt": "",
+            "seedid": "",
+            "genid": "",
+            "imagefile": "",
+            "imageurl": ""
+        },
+        "page8": {
+            "narrative": "",
+            "imageprompt": "",
+            "seedid": "",
+            "genid": "",
+            "imagefile": "",
+            "imageurl": ""
+        },
+        "page9": {
+            "narrative": "",
+            "imageprompt": "",
+            "seedid": "",
+            "genid": "",
+            "imagefile": "",
+            "imageurl": ""
+        },
+        "page10": {
+            "narrative": "",
+            "imageprompt": "",
+            "seedid": "",
+            "genid": "",
+            "imagefile": "",
+            "imageurl": ""
+        }
+    }
+}
+    
+
+
+def initialize_session_states():
+
+  if "storydata" not in st.session_state:
+    st.session_state.storydata = storydata_template
+
+  if "title" not in st.session_state:
+    st.session_state.title = ""
+
+  if "summary" not in st.session_state:
+    st.session_state.summary = ""
+
+  if "character_description" not in st.session_state:
+    st.session_state.character_description = ""
+
