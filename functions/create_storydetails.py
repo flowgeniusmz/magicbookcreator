895177d4a295aba99a9df1f22fb9cbfc7fc2e7cb
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 14:21:46 -0600

    Update create_storydetails.py

--- functions/create_storydetails.py
+++ functions/create_storydetails.py
@@ -3,16 +3,17 @@ from openai import OpenAI
 from functions import update_storydata as sdata
 from utils import openai_assistant as oaiAsst
 
+client = OpenAI(api_key = st.secrets.openai.api_key)
+assistantid = "asst_f9LcmBeJRI1VeVk4d1gQ0V79"
+threadid = client.beta.threads.create().id
+storybookdata = sdata.get_story_data_nopages()
 
-def create_story_details():
-  client = OpenAI(api_key = st.secrets.openai.api_key)
-  assistantid = "asst_f9LcmBeJRI1VeVk4d1gQ0V79"
-  threadid = client.beta.threads.create().id
-  storybookdata = sdata.get_story_data_nopages()
+def create_story_details(varStoryData):
+  
   instructions = f"""
   Create a 10-page storybook outline, narrative, and illustration prompts using the following details:
   
-  {storybookdata}
+  {varStoryData}
   
   REMINDER: Only return the final json output.
   """
