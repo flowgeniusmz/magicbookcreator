a1868c58b653099ede953a5e78162e8109149fc4
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 04:32:43 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ from character_description import get_character_description
 from temp_file import get_tempfile_path
 from generate_image import create_image
 from story_data import update_story_data
-from pagesetup_buildsummary import get_buildsummary1
+
 
 st.set_page_config(page_title = "MagicBook Creator", page_icon = "📚", layout = "wide", initial_sidebar_state = "collapsed")
 st.title('Storybook Creator')
@@ -75,7 +75,7 @@ with tab_INI:
 
 with tab_AI:
     #st.write('ai chat will receive the elements inputs')
-    get_buildsummary1()
+    
 with tab_BS:
     
     container = st.container(border=True)
