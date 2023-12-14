1133460726abc3d6820eb9ca87549cd8cc980043
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 14:08:09 -0600

    Update create_storydetails.py

--- functions/create_storydetails.py
+++ functions/create_storydetails.py
@@ -4,7 +4,7 @@ from functions import update_storydata as sdata
 from utils import openai_assistant as oaiAsst
 
 
-def create_story_details(
+def create_story_details():
   client = OpenAI(api_key = st.secrets.openai.api_key)
   assistantid = "asst_f9LcmBeJRI1VeVk4d1gQ0V79"
   threadid = client.beta.threads.create().id
