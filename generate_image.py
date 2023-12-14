4b91b56ae5e56474409315b4946849b02ad63bc9
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 03:14:00 -0600

    Create generate_image.py

---
+++ generate_image.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from openai import OpenAI
+import json
+
+client = OpenAI(api_key = st.secrets.openai.api_key)
+model = "dall-3-3"
+size = "1024x1024"
+quality = "standard"
+
+def create_image(varPrompt):
+  image_response = client.images.generate(
+    model=model,
+    prompt=varPrompt,
+    size=size,
+    quality=quality,
+    n=1
+  )
+
+  generated_image_url = image_response['data'][0]['url']
+  return generated_image_url
+
+
