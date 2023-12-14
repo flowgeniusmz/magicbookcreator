98740372d26e27ae236a1a2f777e6ff5a969883f
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 14:20:55 -0600

    Update wizard_AI.py

--- apps/wizard_AI.py
+++ apps/wizard_AI.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from openai import OpenAI
-from functions import create_storydetails as genstory
+from functions import create_storydetails as genstory, update_storydata as sdata
+
+storybook_data = st.session_state.storydata
 
 def app_wizard_AI():
-  wizardstoryoutline = genstory.create_story_details()
+  st.write(storybook_data)
+  wizardstoryoutline = genstory.create_story_details(storybook_data)
   st.write(wizardstoryoutline)
