a280d81f85f11cf1c3a2a2030b70343aca8b70d9
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 17:22:43 -0600

    Update sessionstates.py

--- config/sessionstates.py
+++ config/sessionstates.py
@@ -106,6 +106,36 @@ storydata_template = {
 
 
 def initialize_session_states():
+    # Initialize 'storydata' if not present in session state
+    if "storydata" not in st.session_state:
+        st.session_state.storydata = storydata_template
+
+    # Initialize top-level elements
+    for key in ['title', 'summary', 'pagecount']:
+        if key not in st.session_state:
+            st.session_state[key] = storydata_template[key]
+
+    # Initialize 'character' elements
+    character_keys = ['provided_image_url', 'character_description', 'character_name', 'character_relation']
+    for key in character_keys:
+        if f"character_{key}" not in st.session_state:
+            st.session_state[f"character_{key}"] = storydata_template['character'][key]
+
+    # Initialize 'storyelements' elements
+    storyelements_keys = ['genre', 'setting', 'supportingcharacter', 'plotelements', 'selectedtheme', 'magicalobjects', 'tonemood']
+    for key in storyelements_keys:
+        if f"storyelements_{key}" not in st.session_state:
+            st.session_state[f"storyelements_{key}"] = storydata_template['storyelements'][key]
+
+    # Initialize 'pages' elements
+    for i in range(1, storydata_template['pagecount'] + 1):
+        page_keys = ['narrative', 'imageprompt', 'seedid', 'genid', 'imagefile', 'imageurl']
+        for key in page_keys:
+            if f"page{i}_{key}" not in st.session_state:
+                st.session_state[f"page{i}_{key}"] = storydata_template['pages'][f'page{i}'][key]
+
+
+def initialize_session_states_old():
     if "storydata" not in st.session_state:
         st.session_state.storydata = storydata_template
     if "title" not in st.session_state:
