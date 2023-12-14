186e3cc2ab07e6ae3184ebd3f129196e5bba962c
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 10:23:54 -0600

    Update update_storydata.py

--- functions/update_storydata.py
+++ functions/update_storydata.py
@@ -4,12 +4,13 @@ import streamlit as st
 def update_story_data(key, value):
     """
     Update the story_data JSON with the provided key and value.
-    
+
     Args:
         key (str): The key where the value should be updated.
         value: The value to update.
     """
-    if key in story_data:
-        story_data[key] = value
+    if key in st.session_state.storydata:
+        st.session_state.storydata[key] = value
     else:
         print(f"Key '{key}' not found in story_data JSON.")
+
