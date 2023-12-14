a4de147aea95adeb946932e16c9980bf557ab2c1
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 14:35:33 -0600

    Update wizard_AI.py

--- apps/wizard_AI.py
+++ apps/wizard_AI.py
@@ -3,14 +3,14 @@ from openai import OpenAI
 from functions import create_storydetails as genstory, update_storydata as sdata
 
 if "awAI" not in st.session_state:
-  st.session_state.awAI = False
+  st.session_state.awAI = 0
 
 wINIcomplete = st.session_state.awINI
 
 storybook_data = st.session_state.storydata
 
 def app_wizard_AI():
-  if wINIcomplete:
+  if wINIcomplete==1
     st.write(storybook_data)
     wizardstoryoutline = genstory.create_story_details(storybook_data)
     st.write(wizardstoryoutline)
