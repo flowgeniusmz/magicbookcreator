4f71e0cf33e6d2f87f25ab2081256a6a8266b776
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 14:37:52 -0600

    Update wizard_AI.py

--- apps/wizard_AI.py
+++ apps/wizard_AI.py
@@ -2,18 +2,12 @@ import streamlit as st
 from openai import OpenAI
 from functions import create_storydetails as genstory, update_storydata as sdata
 
-if "awAI" not in st.session_state:
-  st.session_state.awAI = 0
-
-wINIcomplete = st.session_state.awINI
 
 storybook_data = st.session_state.storydata
 
 def app_wizard_AI():
-  if wINIcomplete=1
-    st.write(storybook_data)
-    wizardstoryoutline = genstory.create_story_details(storybook_data)
-    st.write(wizardstoryoutline)
-  else:
-    st.write("Complete INI page")
- 
+  
+  st.write(storybook_data)
+  wizardstoryoutline = genstory.create_story_details(storybook_data)
+  st.write(wizardstoryoutline)
+  
