1bb4864370088f7675e352736b93548431364e66
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 13:15:08 -0600

    Update wizard_BS.py

--- apps/wizard_BS.py
+++ apps/wizard_BS.py
@@ -1,40 +1,35 @@
 import streamlit as st
 
 # Helper function for creating narrative and illustration containers
-def create_narrative_illustration_container(page_num):
-    def even(page_num):
-        cols = st.columns([3, 1, 3])
-        with cols[0]:
-            exp_narrative = st.expander(f"Page {page_num} Narrative", expanded=True)
-            with exp_narrative:
-                st.markdown("Narrative")
-        with cols[2]:
-            exp_illustration = st.expander(f"Page {page_num} Illustration", expanded=True)
-            with exp_illustration:
-                st.markdown("Illustration")
-
-    def odd(page_num):
-        cols = st.columns([3, 1, 3])
-        with cols[0]:
-            exp_illustration = st.expander(f"Page {page_num} Illustration", expanded=True)
-            with exp_illustration:
-                st.markdown("Illustration")
-        with cols[2]:
-            exp_narrative = st.expander(f"Page {page_num} Narrative", expanded=True)
-            with exp_narrative:
-                st.markdown("Narrative")
-
-    if page_num % 2 == 0:
-        even(page_num)
-    else:
-        odd(page_num)
-
+def create_narrative_illustration_container_odd_even(page_num):
+    container = st.container()  # Create a container for each page
+    with container:
+        if page_num % 2 == 0:
+            cols = st.columns([3, 1, 3])
+            with cols[0]:
+                exp_narrative = st.expander(f"Page {page_num} Narrative", expanded=True)
+                with exp_narrative:
+                    st.markdown("Narrative")
+            with cols[2]:
+                exp_illustration = st.expander(f"Page {page_num} Illustration", expanded=True)
+                with exp_illustration:
+                    st.markdown("Illustration")
+        else:
+            cols = st.columns([3, 1, 3])
+            with cols[0]:
+                exp_illustration = st.expander(f"Page {page_num} Illustration", expanded=True)
+                with exp_illustration:
+                    st.markdown("Illustration")
+            with cols[2]:
+                exp_narrative = st.expander(f"Page {page_num} Narrative", expanded=True)
+                with exp_narrative:
+                    st.markdown("Narrative")
 
 import streamlit as st
 
-# Define your get_wizard_BS function to display containers
+# Define your app_wizard_BS function to display containers
 def app_wizard_BS():
     for page_num in range(1, 11):
-        create_narrative_illustration_container(page_num)
+        create_narrative_illustration_container_odd_even(page_num)
     st.write("Your storybook's narrative and pages in order:")
     st.button("Finalize the Story!", key="btnFinalizeStory")
