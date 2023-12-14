a75a0934404365a0adc2f68087f976744e82bd5c
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 04:56:38 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -80,21 +80,91 @@ with tab_AI:
     
 with tab_BS:
     
-    container = st.container(border=True)
+    container0 = st.container(border=True)
+    with container0: 
+        st.title('Title Placeholder')
+        st.markdown('Summary Placeholder')
+        
+    container1 = st.container(border=True)
+    with container1: 
+        cc1 = st.columns([3,1,3])
+        with cc1[0]:
+            exp11 = st.expander("Page 1 Narrative", expanded = True)
+        with cc1[2]: 
+            exp12 = st.expander("Page 1 Illustration", expanded = True)
+        
     container2 = st.container(border=True)
+    with container2: 
+        cc2 = st.columns([3,1,3])
+        with cc2[0]:
+            exp21 = st.expander("Page 2 Narrative", expanded = True)
+        with cc2[2]: 
+            exp22 = st.expander("Page 2 Illustration", expanded = True)
+        
     container3 = st.container(border=True)
+    with container3: 
+        cc3 = st.columns([3,1,3])
+        with cc3[0]:
+            exp31 = st.expander("Page 3 Narrative", expanded = True)
+        with cc3[2]: 
+            exp32 = st.expander("Page 3 Illustration", expanded = True)
+            
     container4 = st.container(border=True)
+    with container4: 
+        cc4 = st.columns([3,1,3])
+        with cc4[0]:
+            exp41 = st.expander("Page 4 Narrative", expanded = True)
+        with cc4[2]: 
+            exp42 = st.expander("Page 4 Illustration", expanded = True)
+            
     container5 = st.container(border=True)
-
-    container.title('Title placeholder')
-    container2.write("Page 1 Image")
-    container2.write("Page 2 Narrative")
-    container3.write("Page 1 Image")
-    container3.write("Page 2 Narrative")
-    container4.write("Page 1 Image")
-    container4.write("Page 2 Narrative")
-    container5.write("Page 1 Image")
-    container5.write("Page 2 Narrative")
+    with container5: 
+        cc5 = st.columns([3,1,3])
+        with cc5[0]:
+            exp51 = st.expander("Page 5 Narrative", expanded = True)
+        with cc5[2]: 
+            exp52 = st.expander("Page 5 Illustration", expanded = True)
+            
+    container6 = st.container(border=True)
+    with container6: 
+        cc6 = st.columns([3,1,3])
+        with cc6[0]:
+            exp61 = st.expander("Page 6 Narrative", expanded = True)
+        with cc6[2]: 
+            exp62 = st.expander("Page 6 Illustration", expanded = True)
+            
+    container7 = st.container(border=True)
+    with container7: 
+        cc7 = st.columns([3,1,3])
+        with cc7[0]:
+            exp71 = st.expander("Page 7 Narrative", expanded = True)
+        with cc7[2]: 
+            exp72 = st.expander("Page 7 Illustration", expanded = True)
+            
+    container8 = st.container(border=True)
+    with container8: 
+        cc8 = st.columns([3,1,3])
+        with cc8[0]:
+            exp81 = st.expander("Page 8 Narrative", expanded = True)
+        with cc8[2]: 
+            exp82 = st.expander("Page 8 Illustration", expanded = True)
+            
+    container9 = st.container(border=True)
+    with container9: 
+        cc9 = st.columns([3,1,3])
+        with cc9[0]:
+            exp92 = st.expander("Page 9 Narrative", expanded = True)
+        with cc9[2]: 
+            exp91 = st.expander("Page 9 Illustration", expanded = True)
+            
+    container10 = st.container(border=True)
+    with container10: 
+        cc10 = st.columns([3,1,3])
+        with cc10[0]:
+            exp101 = st.expander("Page 10 Narrative", expanded = True)
+        with cc10[2]: 
+            exp102 = st.expander("Page 10 Illustration", expanded = True)
+    
 
     st.write("Your storybook's narrative and pages in order:")
 
