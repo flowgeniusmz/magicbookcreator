c7db9cf7d467d5db450ea19495d0c5485050d7eb
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 14:04:23 -0600

    Update wizard_AI.py

--- apps/wizard_AI.py
+++ apps/wizard_AI.py
@@ -1,1 +1,7 @@
+import streamlit as st
+from openai import OpenAI
+from functions import create_storydetails as genstory
 
+def app_wizard_AI():
+  wizardstoryoutline = genstory.create_story_details()
+  st.write(storyoutline)
