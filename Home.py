d0873ecca470274c4a3636e1966639f871defe48
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-15 16:58:46 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,18 +3,16 @@ from apps import wizard_INI as wINI, wizard_BS as wBS, wizard_AI as wAI
 from config import sessionstates as ss, pagesetup as ps
 from PIL import Image
 
-
-# 0. App Initialization and Setup
+# App Initialization and Setup
 st.set_page_config(page_title="MagicBook Creator", page_icon="📚", layout="wide", initial_sidebar_state="collapsed")
 
-if "initialized" not in st.session_state:
-    st.session_state.initialized = True
-    ss.initialize_session_states()
-    
+# Initialize session states
+ss.initialize_session_states()
+
 ps.set_title("MagicBook", "Creator")
 ps.set_page_overview("Welcome to MagicBook Creator!", "MagicBook Creator creates a magical storybook...")
 
-# 1. Wizard Form Initialization
+# Wizard Form Initialization
 tab_INI, tab_AI, tab_BS, tab_DS = st.tabs(["Initial Story Elements", "AI Story Builder", "Build Summary", "Download Story"])
 
 with tab_INI:
@@ -26,24 +24,23 @@ with tab_AI:
 
 with tab_BS:
     wBS.app_wizard_BS()
-    st.write(st.session_state.threadid)
 
 # Additional code for other tabs and functionality
 
-cont = st.container(border=True)
+# Container for displaying uploaded and generated images
+cont = st.container()
 with cont:
-    if "uploadedimage" not in st.session_state or st.session_state.uploadedimage == "":
-        warning_noimage = st.warning("Please upload an image to continue")
-    else:
+    if 'uploadedimage' in st.session_state and st.session_state.uploadedimage:
         expd = st.expander("Character Images", expanded=True)
         with expd:
-            col = st.columns ([3,1,3])
+            col = st.columns([3, 1, 3])
             with col[0]:
                 openimage = Image.open(st.session_state.uploadedimage)
                 st.image(openimage, "Uploaded Image")
             with col[2]:
-                openimage2 = Image.open(st.session_state.storydata["character"]["character_image"])
-                st.image(openimage2, "Created Image")
-            st.markdown(f"""**Character Description:** {st.session_state.storydata["character"]["character_description"]}""")
-            
-            
+                if 'character_image' in st.session_state.storydata['character']:
+                    openimage2 = Image.open(st.session_state.storydata['character']['character_image'])
+                    st.image(openimage2, "Created Image")
+            st.markdown(f"""**Character Description:** {st.session_state.storydata['character']['character_description']}""")
+    else:
+        st.warning("Please upload an image to continue.")
