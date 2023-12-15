eb5275640757dba7e30c0c1a1acc4ca601df2f85
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-14 22:57:25 -0600

    Update wizard_AI.py

--- apps/wizard_AI.py
+++ apps/wizard_AI.py
@@ -1,28 +1,21 @@
 import streamlit as st
 import openai
 from openai import OpenAI
+from functions import get_storydetails as sdetails
 
 client = OpenAI(api_key = st.secrets.openai.api_key)
 
 
 def app_wizard_AI():
     st.write("AI Story Builder")
-    st.write(st.session_state.storydata)
-    # Check if story details are available in session state
-    if 'storydatadetails' in st.session_state and st.session_state.storydatadetails is not None:
-        story_details = st.session_state.storydatadetails
-
-        # Display the story details
-        st.write("Here are your story details:")
-        st.json(story_details)  # Display the story details in JSON format, or format as needed
-
-        # Additional code to allow further editing or processing of story details
-        # ...
-
-    else:
-        st.warning("Please complete the Initial Story Elements to generate the story details.")
-        # Optionally, provide a button or link to go back to the Initial Story Elements tab
-        # ...
-
+    btnGetStoryDetails = st.button("Get Story Details", key="btnStoryDetails")
+    if btnGetStoryDetails:
+        if "storydatadetails" not in st.session_state:
+            storydetails = sdetails.create_story_details(st.session_state.storydata)
+            st.session_state.storydatadetails = storydetails
+            st.write(st.session_state.storydatadetails)
+        else:
+            st.write(st.session_state.storydatadetails)
+    
    
 
